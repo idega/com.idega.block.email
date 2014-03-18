97284d58a0bc6973b5aae6774be9ad223de459ac
Valdas Žemaitis <valdas@idega.com>
2014-03-18 10:09:18 +0200

    Bug fix

--- src/java/com/idega/block/email/patterns/DefaultSubjectPatternFinder.java
+++ src/java/com/idega/block/email/patterns/DefaultSubjectPatternFinder.java
@@ -89,7 +89,7 @@ public abstract class DefaultSubjectPatternFinder implements EmailSubjectSearcha
 
 	public void addPattern(Pattern pattern) {
 		if (!patterns.contains(pattern)) {
-			addPattern(pattern);
+			patterns.add(pattern);
 		}
 	}
 
